80819c4e69465aa61a7adc46b61c65f58ce9ccf7
木生 <Li_Peifan@outlook.com>
2018-02-07 22:00:48 +0800

    Create 301.c
    the code of 301

---
+++ s3/301.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+        int a;
+        float b, c;
+
+        a = 12345678901234;
+        b = 1234567890.12345;
+        c = 0.000023326464;
+
+        printf("%d  %f  %f\n",a, b, c);
+
+        return 0;
+}
