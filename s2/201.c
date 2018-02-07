00362e125db71778032344b60807097afb7a119a
木生 <Li_Peifan@outlook.com>
2018-02-07 16:20:16 +0800

    201.c
    the code of 201(My first code in c)

---
+++ s2/201.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+
+int main(void)
+{
+        printf("Li Peifan\n");
+        printf("Li\nPeifan\n");
+        printf("Li ");
+        printf("Peifan\n");
+
+        return 0;
+}
