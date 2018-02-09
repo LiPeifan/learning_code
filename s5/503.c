9cd17465e66781d3b9041d7f73326189acaebb0c
木生 <Li_Peifan@outlook.com>
2018-02-09 16:41:15 +0800

    Update 503.c
    the code of 503

--- s5/503.c
+++ s5/503.c
@@ -7,9 +7,14 @@ int main(void)
         int week,day;
         printf("please enter the num of days:\n");
         scanf("%d", &day0);
-        week = day0 / D_PER_W;
-        day = day0 % D_PER_W;
-        printf("%d days are %d weeks, %d days.\n", day0, week, day);
+        while (day0 > 0)
+        {
+                week = day0 / D_PER_W;
+                day = day0 % D_PER_W;
+                printf("%d days are %d weeks, %d days.\n", day0, week, day);
+                printf("the next?(enter a num <= 0 to quit)\n");
+                scanf("%d", &day0);
+        }
 
         return 0;
 }
